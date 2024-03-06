7e945c15ac42e47a899995fca20eb9a40ade19a3
strasse34 <rmirzaie2021@gmail.com>
2024-03-06 15:08:20 +0000

    bug: I can follow myself

---
+++ followers/models.py
@@ -0,0 +1,37 @@
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Follower(models.Model):
+    """
+    Follower model, related to 'owner' and 'followed'.
+    'owner' is a User that is following a User.
+    'followed' is a User that is followed by 'owner'.
+    We need the related_name attribute so that Django can differentiate
+    between 'owner' and 'followed' who are both User model instances.
+    'unique_together' makes sure a user can't 'double follow' the same user.
+    """
+    owner = models.ForeignKey(
+        User, related_name='following', on_delete=models.CASCADE
+    )
+    followed = models.ForeignKey(
+        User, related_name='followed', on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'followed']
+
+    def clean(self):
+        if self.owner == self.followed:
+            raise ValidationError("You cannot follow yourself.")
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.owner} is following {self.followed}'
+
---
+++ followers/views.py
@@ -0,0 +1,25 @@
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
+
+
+class FollowerList(generics.ListCreateAPIView):
+    """
+    List of all followers
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a follower    
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
