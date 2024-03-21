4f0f095dbd0200d59c472e3cb0e391e941f3fa0d
strasse34 <rmirzaie2021@gmail.com>
2024-03-21 18:48:47 +0000

    I get unauthorised message in front, checked views and other authentications matters.

--- comments/views.py
+++ comments/views.py
@@ -1,24 +1,18 @@
-from rest_framework import generics, permissions, filters
+from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
-from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CommentList(generics.ListCreateAPIView):
     """
-    List of comments or create a comment if logged in.
+    List comments or create a comment if logged in.
     """
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
-
-    filter_backends = [
-        filters.OrderingFilter,
-        DjangoFilterBackend,
-    ]
-
-    # Filters the comments by post.
+    filter_backends = [DjangoFilterBackend]
     filterset_fields = ['post']
 
     def perform_create(self, serializer):
@@ -32,11 +26,3 @@ class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = CommentDetailSerializer
     queryset = Comment.objects.all()
-
-    def get_serializer_class(self):
-        """
-        Return the appropriate serializer class based on the request method.
-        """
-        if self.request.method == 'PUT' or self.request.method == 'PATCH':
-            return CommentSerializer
-        return CommentDetailSerializer
